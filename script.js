dfa0b7dcd375665790bfb31934028f784282a1ae
d hendrix <104184081+dHendrixSeven@users.noreply.github.com>
2022-12-23 04:43:25 -0600

    s

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let nav = document.getElementById("navbar")
+let menu = document.getElementById("menu")
+let collapsed = document.getElementById("collapsed")
+let unclicked = true
 
+menu.onclick = function(){
+    if(unclicked){
+        unclicked = false;
+        collapsed.style.display = "flex"
+    }
+    else {
+        collapsed.style.display = "none"
+        unclicked = true
+    }
+}
